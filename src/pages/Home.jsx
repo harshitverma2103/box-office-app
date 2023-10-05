160794b8c396e3d034c477d6f16be002b522310e
harshitverma2103 <harshitverma2103@gmail.com>
2023-10-05 23:47:49 +0530

    isolated search form logic in searchForm component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,23 @@
 import { useState } from 'react';
 import { searchForShows, searchForPeople } from './../api/tvmaze';
+import SearchForm from '../Component/SearchForm';
 
 const Home = () => {
-  const [searchStr, setSearchStr] = useState('');
   const [apiData, setApiData] = useState(null);
   const [apiDataError, setApiDataError] = useState(null);
-  const [searchOption, setSearchOption] = useState('show');
 
-  const onSearchInputChange = ev => {
-    setSearchStr(ev.target.value);
-  };
-
-  const onRadioChange = ev => {
-    setSearchOption(ev.target.value);
-  };
-
-  const onSearch = async ev => {
-    ev.preventDefault();
-
+  const onSearch = async ({ q, searchOption }) => {
     try {
       setApiDataError(null);
 
+      let result;
       if (searchOption === 'shows') {
-        const result = await searchForShows(searchStr);
-        setApiData(result);
+        result = await searchForShows(q);
       } else {
-        const result = await searchForPeople(searchStr);
-        setApiData(result);
+        result = await searchForPeople(q);
       }
+
+      setApiData(result);
     } catch (error) {
       setApiDataError(error);
     }
@@ -35,14 +25,14 @@ const Home = () => {
 
   const renderApiData = () => {
     if (apiDataError) {
-      return <div> Error occured: {apiDataError.message} </div>;
+      return <div>Error occurred: {apiDataError.message}</div>;
     }
 
     if (apiData) {
       return apiData[0].show
-        ? apiData.map(data => <div key={data.show.id}>{data.show.name} </div>)
+        ? apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
         : apiData.map(data => (
-            <div key={data.person.id}>{data.person.name} </div>
+            <div key={data.person.id}>{data.person.name}</div>
           ));
     }
 
@@ -51,33 +41,7 @@ const Home = () => {
 
   return (
     <div>
-      <form onSubmit={onSearch}>
-        <input type="text" value={searchStr} onChange={onSearchInputChange} />
-
-        <label>
-          Shows
-          <input
-            type="radio"
-            name="search-option"
-            value="shows"
-            checked={searchOption === 'shows'}
-            onChange={onRadioChange}
-          />
-        </label>
-
-        <label>
-          Actors
-          <input
-            type="radio"
-            name="search-option"
-            value="actors"
-            checked={searchOption === 'actors'}
-            onChange={onRadioChange}
-          />
-        </label>
-
-        <button type="submit">Search</button>
-      </form>
+      <SearchForm onSearch={onSearch} />
       <div>{renderApiData()}</div>
     </div>
   );
